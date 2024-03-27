37225192fbfeaf85fa83fffd6c1250c084720e5d
FBDev <elzadam11@gmail.com>
2024-03-27 16:40:03 +0100

    Update fbd.sh
    Signed-off-by: FBDev <elzadam11@gmail.com>

--- src/fbd.sh
+++ src/fbd.sh
@@ -2,7 +2,6 @@
 
 # Customize Console
 # set-title FBD
-
 echo "Undertale Jokes, that's all. Mainly. You know, as ya want."
 
 while true
@@ -11,22 +10,35 @@ while true
     #Input
     VHS=$(gum input > history.txt --placeholder " Enter Command")
 
-    # Logic
-    if "$VHS" '==' "list"; then
+    # Treat Inpuit
+    if [ "$VHS" '==' "list" ]; then
         echo "exit"
         echo "list"
         echo "rules"
     fi
-    if "$VHS" '==' "exit"; then
+    if [ "$VHS" '==' "exit" ]; then
         exit
     fi
-    if "$VHS" '==' "rules"; then
+    if [ "$VHS" '==' "rules" ]; then
         echo "There is not much rules but :\n
         1. Be silly and trolly
         2. Don't insult or any things like that
         3. Always have a Sans sticker in your desktop wallpaper
         4. Chew gum while using the software
-        5. Every Arch user shoulds say that he uses arch BTW"
+        5. Every Arch user shoulds ay "I use Arch, BTW" every day on the chat.\n"
     fi
-
-done
+    if [ "$VHS" '==' "cls" ]; then
+        clear
+    fi
+    if [ "$VHS" '==' "issue" ]; then
+        echo "Go to github.com/fbdev64/FBD/issues and report it."
+    fi
+    if [ "$VHS" '==' "sans" ]; then
+        gum pager ./jokes/sans.txt
+    fi
+    if [ "$VHS" '==' "puns" ]; then
+        ggu pager ./jokes/puns.txt
+    fi
+    if [ "$VHS" '==' "time" ]; then
+        echo $date
+    fi
