cf6625bde6ab5ba938a808e0b0b537562bd24068
FBDev <elzadam11@gmail.com>
2024-03-27 15:36:12 +0000

    Reworked FBD, change structure

--- src/fbd.sh
+++ src/fbd.sh
@@ -9,24 +9,24 @@ while true
   do 
 
     #Input
-    CMD=$(gum input > history.txt --placeholder " Enter Command")
+    VHS=$(gum input > history.txt --placeholder " Enter Command")
 
     # Logic
-    if [ "$CMD" == "list" ]; then
+    if "$VHS" '==' "list"; then
         echo "exit"
         echo "list"
         echo "rules"
     fi
-    if [ "$CMD" == "exit" ]; then
+    if "$VHS" '==' "exit"; then
         exit
     fi
-    if [ "$CMD"=="rules" ]; then
+    if "$VHS" '==' "rules"; then
         echo "There is not much rules but :\n
         1. Be silly and trolly
         2. Don't insult or any things like that
         3. Always have a Sans sticker in your desktop wallpaper
         4. Chew gum while using the software
-        5. Every Arch user shoulds ay "I use Arch, BTW" every day on the chat.\n"
+        5. Every Arch user shoulds say that he uses arch BTW"
     fi
 
 done
