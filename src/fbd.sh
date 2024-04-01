268ad0998478e2e32c2fac1f72601ac72209f0cd
FBDev <elzadam11@gmail.com>
2024-04-01 20:35:56 +0100

    Update fbd.sh
    Signed-off-by: FBDev <elzadam11@gmail.com>

--- src/fbd.sh
+++ src/fbd.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 # Customize Console
-# set-title FBD
+PROMPT_COMMAND='echo -en "\033]0; $("FBD") \a"'
+now=$(date)
 echo "Undertale Jokes, that's all. Mainly. You know, as ya want."
 
 while true
@@ -20,12 +21,33 @@ while true
 			clear
 			;;
 		puns)
-			gum pager ./jokes/puns.txt
+			gum pager > ./jokes/puns.txt
 			;;
-		other)
-			gum pager ./jokes/sans.txt
+		sans)
+			gum pager > ./jokes/sans.txt
 			;;
-	*)
+		help)
+			echo "clear"
+			echo "cls"
+			echo "date"
+			echo "exit"
+			echo "help"
+			echo "issue"
+			echo "puns"
+			echo "rules"
+			echo "sans"
+			echo "time"
+			;;
+		issue)
+			echo "Report issue at github.com/FBD/issues"
+			;;
+		rules)
+			echo "3 rules : 1. do NOT insult and NO NSFW. 2. BE a troll 3. Arch users should use the command arch on every startup."
+			;;	
+		time | date)
+			echo "$now"
+			;;
+		*)
 		echo -n "Command unknown or not implemented yet."
 		;;
 	esac	
