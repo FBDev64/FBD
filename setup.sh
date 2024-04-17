740226d75dd18d9b6d18f4666ddb7e9c160aabd6
bikobi <bikobi@disroot.org>
2024-04-17 17:44:53 +0100

    Replaced setup with launcher

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-chmod +x ./src/fbd.sh
-sudo pacman -S gum
-gum log --level info "Setup Finished. Before running FBD, test your gum installation. Then, execute the command : ./src/fbd.sh" 
